4e9a9ea53860508ba324d7c32f0841690f825f80
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-11-01 22:06:30 +0300

    реализовал root компонент и добавил навигацию

--- app/src/main/java/ru/sumin/weatherapp/presentation/root/RootComponentImpl.kt
+++ app/src/main/java/ru/sumin/weatherapp/presentation/root/RootComponentImpl.kt
@@ -3,6 +3,10 @@ package ru.sumin.weatherapp.presentation.root
 import android.os.Parcelable
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.router.stack.push
 import com.arkivanov.decompose.value.Value
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
@@ -20,8 +24,15 @@ class RootComponentImpl @AssistedInject constructor(
     private val favouriteComponentFactory: FavouriteComponentImpl.Factory,
     @Assisted("componentContext") componentContext: ComponentContext
 ) : RootComponent, ComponentContext by componentContext {
-    override val stack: Value<ChildStack<*, RootComponent.Child>>
-        get() = TODO("Not yet implemented")
+
+    private val navigation = StackNavigation<Config>()
+
+    override val stack: Value<ChildStack<*, RootComponent.Child>> = childStack(
+        source = navigation,
+        initialConfiguration = Config.Favourite,
+        handleBackButton = true,
+        childFactory = ::child
+    )
 
     private fun child(
         config: Config,
@@ -32,7 +43,7 @@ class RootComponentImpl @AssistedInject constructor(
                 val component = detailsComponentFactory.create(
                     city = config.city,
                     onBackClicked = {
-
+                        navigation.pop()
                     },
                     componentContext = componentContext
                 )
@@ -41,13 +52,13 @@ class RootComponentImpl @AssistedInject constructor(
             Config.Favourite -> {
                 val component = favouriteComponentFactory.create(
                     onCityItemClicked = {
-
+                        navigation.push(Config.Details(it))
                     },
                     onAddFavouriteClicked = {
-
+                        navigation.push(Config.Search(OpenReason.AddToFavourite))
                     },
                     onSearchClicked = {
-
+                        navigation.push(Config.Search(OpenReason.RegularSearch))
                     },
                     componentContext = componentContext
                 )
@@ -57,13 +68,13 @@ class RootComponentImpl @AssistedInject constructor(
                 val component = searchComponentFactory.create(
                     openReason = config.openReason,
                     onCityClicked = {
-
+                        navigation.push(Config.Details(it))
                     },
                     onBackClicked = {
-
+                        navigation.pop()
                     },
                     onSavedToFavourite = {
-
+                        navigation.pop()
                     },
                     componentContext = componentContext
                 )
--- app/src/main/java/ru/sumin/weatherapp/presentation/root/RootContent.kt
+++ app/src/main/java/ru/sumin/weatherapp/presentation/root/RootContent.kt
@@ -1,7 +1,21 @@
 package ru.sumin.weatherapp.presentation.root
 
 import androidx.compose.runtime.Composable
+import com.arkivanov.decompose.extensions.compose.jetpack.stack.Children
+import ru.sumin.weatherapp.presentation.details.DetailsContent
+import ru.sumin.weatherapp.presentation.favourite.FavouriteContent
+import ru.sumin.weatherapp.presentation.search.SearchContent
+import ru.sumin.weatherapp.presentation.ui.theme.WeatherAppTheme
 
 @Composable
 fun RootContent(component: RootComponent) {
+    WeatherAppTheme {
+        Children(stack = component.stack) {
+            when(val instance = it.instance){
+                is RootComponent.Child.Details -> DetailsContent(component = instance.component)
+                is RootComponent.Child.Favourite -> FavouriteContent(component = instance.component)
+                is RootComponent.Child.Search -> SearchContent(component = instance.component)
+            }
+        }
+    }
 }
