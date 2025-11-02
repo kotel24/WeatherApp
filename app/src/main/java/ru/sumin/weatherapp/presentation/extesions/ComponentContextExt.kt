02ca1c441917eb1534e5d1317b99ac45ab7fafd3
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-11-02 11:44:41 +0300

    добавил эксеншин функции для красивого преобразования температуры и даты

--- app/src/main/java/ru/sumin/weatherapp/presentation/extesions/ComponentContextExt.kt
+++ app/src/main/java/ru/sumin/weatherapp/presentation/extesions/ComponentContextExt.kt
@@ -6,9 +6,25 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import kotlin.math.roundToInt
 
 fun ComponentContext.componentScope() = CoroutineScope(
     Dispatchers.Main.immediate+ SupervisorJob()
 ).apply {
     lifecycle.doOnDestroy { cancel() }
+}
+
+fun Float.tempToFormattedString(): String = "${roundToInt()}°C"
+
+fun Calendar.formattedFullDate(): String {
+    val format = SimpleDateFormat("EEEE | d MMM y", Locale.getDefault())
+    return format.format(time)
+}
+
+fun Calendar.formattedShortDayOfWeek(): String {
+    val format = SimpleDateFormat("EEE", Locale.getDefault())
+    return format.format(time)
 }
